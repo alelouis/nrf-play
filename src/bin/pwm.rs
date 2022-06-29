05ee29f9a5ecc66f155f1cee4b4cb95f449aed8d
alelouis <me@alelouis.eu>
2022-06-29 21:15:17 +0200

    Adds PWM control for sg90 motor http://www.ee.ic.ac.uk/pcheung/teaching/DE1_EE/stores/sg90_datasheet.pdf

---
+++ src/bin/pwm.rs
@@ -0,0 +1,53 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use rtt_target::{rprintln, rtt_init_print};
+use panic_rtt_target as _;
+
+use microbit::hal::{Saadc, Timer};
+use microbit::hal::saadc::SaadcConfig;
+use microbit::hal::pac::Peripherals;
+use microbit::hal::gpio;
+use heapless::Vec;
+use microbit::hal::gpio::Level;
+
+
+use microbit::hal::prelude::*;
+
+mod serial_setup;
+use serial_setup::UartePort;
+
+use microbit::hal::pwm;
+use microbit::hal::time::Hertz;
+
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = Peripherals::take().expect("Couldn't initialize board.");
+
+    // Configuring output pin
+    let gpio = gpio::p0::Parts::new(board.P0);
+    let pwm_pin = gpio.p0_02.into_push_pull_output(Level::Low).degrade();
+
+    // Configuring output pin
+    let pwm_motor = pwm::Pwm::new(board.PWM0);
+    pwm_motor.set_output_pin(pwm::Channel::C0, pwm_pin);
+
+    // Using a timer for delayed commands
+    let mut timer = Timer::new(board.TIMER0);
+    
+    pwm_motor.set_period(Hertz(50));
+    pwm_motor.enable_channel(pwm::Channel::C0);
+    pwm_motor.loop_inf();
+
+    let mut duty = 0_u16;
+    loop {
+        rprintln!("{}", duty);
+        timer.delay_ms(1000_u32);
+        duty += 1000;
+        duty = duty % 30000;
+        pwm_motor.set_duty_on(pwm::Channel::C0, duty);
+    }
+}
